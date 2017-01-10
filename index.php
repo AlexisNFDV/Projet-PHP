c771013c2c83047b0ffaf3f3a61f4ed55b06d91b
Alexis Noel <noel.a@hotmail.fr>
2017-01-10 11:59:10 +0100

    First

--- index.php
+++ index.php
@@ -1,21 +1,38 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: Benoît
- * Date: 10/01/2017
- * Time: 10:55
- */
+
+require 'connect.php';
+
 ?>
 <!doctype html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-</body>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+              integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="CSS/style.css">
+
+        <title>Document</title>
+    </head>
+
+
+    <body>
+
+        <header>
+
+        </header>
+
+
+        <footer>
+
+        </footer>
+
+    </body>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 </html>

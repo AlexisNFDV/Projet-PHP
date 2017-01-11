68439ca1198735e5517ab54c6e9fa1fcf196f56d
Alexis Noel <noel.a@hotmail.fr>
2017-01-11 09:16:21 +0100

    Fin journée 1

--- index.php
+++ index.php
@@ -10,7 +10,6 @@ require 'connect.php';
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<<<<<<< HEAD
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -18,29 +17,8 @@ require 'connect.php';
 
     <title>8GAG</title>
 </head>
-=======
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="CSS/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
-
-    <title>Document</title>
-</head>
-
->>>>>>> 41d9982d2d25da388cd1e2a211d25763e742c3d1
 
 <body>
-
-<<<<<<< HEAD
-<body>
-
-<header>
-
-</header>
-=======
-<header>
-
-</header>
 
 <div class="container">
     <h1 class="text-center ent">8GAG</h1>
@@ -105,30 +83,21 @@ require 'connect.php';
             </div>
         </div>
     </div>
->>>>>>> 41d9982d2d25da388cd1e2a211d25763e742c3d1
 
 </div>
-<footer>
-    <p class="copyright"> © Copyright 2016 </p>
-</footer>
 
-<<<<<<< HEAD
 <footer>
     <p class="copyright"> © Copyright 2016 </p>
     <a href="contact.php" id="contact">Nous contacter</a>
 
 </footer>
 
-</body>
-
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
         integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-=======
-</body>
+
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
         integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
         crossorigin="anonymous"></script>
->>>>>>> 41d9982d2d25da388cd1e2a211d25763e742c3d1
-
+</body>
 </html>

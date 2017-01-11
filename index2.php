fba3d0dc050772c17128b0c3b0c1dd19b0af35bc
Benoit Dillies <benoit.dillies@ynov.com>
2017-01-11 15:18:08 +0100

    eh ouais on est la

--- index2.php
+++ index2.php
@@ -65,6 +65,29 @@ require 'connect.php';
     </div>
 </div>
 
+<footer class="page-footer grey darken-3">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">8GAG</h5>
+                <p class="grey-text text-lighten-4">WebMaster Alexis Noel et Benoît Dillies.</p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Liens</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3" href="https://github.com/AlexisNFDV/Projet-PHP">GitHub</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright grey darken-4">
+        <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="contact.php">Nous contactez</a>
+        </div>
+    </div>
+</footer>
+
 </body>
 <!--Materialize -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>

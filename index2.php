9ad49f1c0f3907f93441abe6bb6ab18b503f2187
Benoit Dillies <benoit.dillies@ynov.com>
2017-01-15 19:28:51 +0100

    blue

--- index2.php
+++ index2.php
@@ -31,6 +31,18 @@ if(isset($_GET['connect'])){
 <?php
 require 'header.php';
 ?>
+<div class="container center">
+<ul class="pagination">
+    <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+    <li class="active grey darken-4"><a href="#!">1</a></li>
+    <li class="waves-effect"><a href="#!">2</a></li>
+    <li class="waves-effect"><a href="#!">3</a></li>
+    <li class="waves-effect"><a href="#!">4</a></li>
+    <li class="waves-effect"><a href="#!">5</a></li>
+    <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+</ul>
+</div>
+
 <div class="container">
     <div class="row">
         <div class="col l10 offset-l1">
